aa085172eeaa1d72c9e256c1c31ec5728d5aa4d1
Thiago Catoto Dias <tcatoto@gmail.com>
2016-08-20 16:18:53 -0300

    ADD - Added sync_tse command

---
+++ django/mdb/candidates/management/commands/sync_tse.py
@@ -0,0 +1,44 @@
+# coding: utf-8
+from sys import stdout
+from django.core.management.base import BaseCommand
+from multiprocessing.pool import ThreadPool
+from candidates.models import Candidate, PoliticalParty
+
+import logging
+import requests
+
+logger = logging.getLogger('mdb')
+
+
+def get(candidate, j):
+    response = requests.get('http://divulgacandcontas.tse.jus.br/divulga/rest/v1/candidatura/buscar/2016/71072/2/candidato/{}'.format(candidate.get('id')))
+    response_json = response.json()
+    if 'FEM' == response_json.get('descricaoSexo'):
+        political_party, created = PoliticalParty.objects.get_or_create(initials=candidate.get('partido').get('sigla'))
+
+        candidate_to_save = Candidate()
+        candidate_to_save.political_party = political_party
+        candidate_to_save.name = candidate.get('nomeCompleto')
+        candidate_to_save.save()
+
+    stdout.write("\r\t\t\t%s " % (j))
+    stdout.flush()
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        thread_pool = ThreadPool(processes=30)
+        print "FILL\t\t\tConsume"
+        i = 0
+        response = requests.get('http://divulgacandcontas.tse.jus.br/divulga/rest/v1/candidatura/listar/2016/71072/2/13/candidatos').json()
+        for candidate in response.get('candidatos')[:100]:  # Remove `[:100]` to import all
+            i = i + 1
+            stdout.write("\r%s" % i)
+            stdout.flush()
+            j = int(i)
+            thread_pool.apply_async(get, (candidate, j))  # async
+            # get(candidate, j)  # sync
+        thread_pool.close()
+        thread_pool.join()
+        print "\nDONE!"
