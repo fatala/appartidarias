580cf20e2d5c3c7d8ef290e9b73f2374de1e7072
Andre Fatala <andre@fatala.local>
2016-08-21 12:52:57 -0300

    adding more code

--- django/mdb/candidates/admin.py
+++ django/mdb/candidates/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
-from candidates.models import Candidate, PoliticalParty, Agenda
+from candidates.models import Candidate, PoliticalParty, Agenda, JobRole
+from django.utils.safestring import mark_safe
+from django.utils.html import format_html_join
 
+class CandidateAdmin(admin.ModelAdmin):
+    readonly_fields = ('picture',)
 
-admin.site.register(Candidate)
+    def picture(self, instance):
+        result = '<img src=%s/>' % instance.picture_url
+        return mark_safe(result)
+
+    picture.short_description = "Foto do candidato"
+
+admin.site.register(Candidate, CandidateAdmin)
 admin.site.register(PoliticalParty)
 admin.site.register(Agenda)
+admin.site.register(JobRole)
--- django/mdb/candidates/models.py
+++ django/mdb/candidates/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class PoliticalParty(models.Model):
     initials = models.CharField(max_length=128, verbose_name='sigla')
     name = models.CharField(max_length=128, verbose_name='nome')
+    number = models.IntegerField(verbose_name='numero')
     directory_national = models.TextField(verbose_name='diretório nacional')
     directory_state = models.TextField(verbose_name='diretório estadual')
     directory_city = models.TextField(verbose_name='diretório municipal')
@@ -28,10 +29,30 @@ class Agenda(models.Model):
         return self.name
 
 
+class JobRole(models.Model):
+    name = models.CharField('Cargo', max_length=128)
+
+    class Meta:
+        verbose_name = 'Cargo'
+        verbose_name_plural = 'Cargos'
+
+    def __str__(self):
+        return self.name
+
+
 class Candidate(models.Model):
+    FEMALE = 'F'
+    MALE = 'M'
+    GENDER_CHOICES = (
+        (FEMALE, 'Feminino'),
+        (MALE, 'Masculino')
+    )
     political_party = models.ForeignKey(PoliticalParty, verbose_name='partido')
     name = models.CharField(max_length=128, verbose_name='Nome')
     number = models.IntegerField(verbose_name='numero')
+    job_role = models.ForeignKey(JobRole, verbose_name='Cargo')
+    picture_url = models.CharField(max_length=256, verbose_name='URL da foto')
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default=FEMALE, verbose_name='sexo')
     agenda = models.ForeignKey(Agenda, verbose_name='Pauta')
     projects = models.TextField(verbose_name='Projetos')
 
